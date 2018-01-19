d15c7cd694f8046794b5e16fd38e723001340679
Hugo Piso <hpiso@clever-age.com>
2018-01-19 16:02:26 +0800

    init commit

---
+++ www/src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     *
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+}
